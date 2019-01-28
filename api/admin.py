29c3c220fb2ba56540b058772f095ea765c89b62
z3by <zoaby.am@gmail.com>
2019-01-28 16:47:47 +0200

    FEAT category& category translation admin dashboard

--- api/admin.py
+++ api/admin.py
@@ -4,17 +4,28 @@ from api.models import Category, Subcategory, Product, Company, Contact, Product
 
 class CategoryAdmin(admin.ModelAdmin):
     model = Category
-    list_display = ['id', 'name', 'description', 'available_translations']
+    list_display = ['name', 'description', 'available_translations']
 
     def available_translations(self, obj):
         t = list(CategoryTranslation.objects.filter(category__id=obj.id))
         return t
 
 
+class CategoryTranslationAdmin(admin.ModelAdmin):
+    model = CategoryTranslation
+    list_display = ['category_name', 'description', 'language_code']
+
+    def language_code(self, obj):
+        return obj.language.code
+
+    def category_name(self, obj):
+        return obj.category.name
+
+
 # Register your models here.
 admin.site.register(Language)
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(CategoryTranslation)
+admin.site.register(CategoryTranslation, CategoryTranslationAdmin)
 admin.site.register(Subcategory)
 admin.site.register(Product)
 admin.site.register(Company)
